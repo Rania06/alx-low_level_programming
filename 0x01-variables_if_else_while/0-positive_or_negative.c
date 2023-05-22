84609948389aa5de48c4ff5dbb80b4e312b36fc6
Rania El Moussoui <rania.elmoussaoui@gmail.com>
2023-05-22 12:53:24 -0700

    comment if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%dis positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 printf("%d is zero\n", n);
@@ -23,3 +23,4 @@ printf("%d is negative\n", n);
 }
 return (0);
 }
+
