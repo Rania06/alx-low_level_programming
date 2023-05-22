67214ad9f81f651fb67577fa48d7d1e2d56802cb
Rania El Moussoui <rania.elmoussaoui@gmail.com>
2023-05-22 13:48:31 -0700

    gggg comment

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+/**
+ * main - task2 alphabet
+ * Return:0 (success)
+ */
+int main(void)
+{
+char alphabett[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar('\n');
+}
+return (0);
+}
