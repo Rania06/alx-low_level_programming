fc3907dab950c92a4982adcbabf9e969abac3672
Rania El Moussoui <rania.elmoussaoui@gmail.com>
2023-05-24 16:11:33 -0700

    project nested

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - fibonacci
+* Return: 0
+*/
+int main(void)
+{
+unsigned long int i;
+unsigned long int be = 1;
+unsigned long int af = 2;
+unsigned long int o= 1000000000;
+}
