0274e7d726d56dd4fd8486375a1eebde87c932f6
Rania El Moussoui <rania.elmoussaoui@gmail.com>
2023-05-29 15:42:49 -0700

    aafak last tasks these

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * char *_strcpy string pointed to by src
+ * @tt: to
+ * @src: from
+ * Return: string
+ */
+char *_strcpy(char *tt, char *src)
+{
+int l = 0;
+int x = 0;
+
+while (*(src + l) != '\0')
+{
+l++;
+}
+for ( ; x < l ; x++)
+{
+tt[x] = src[x];
+}
+tt[l] = '\0';
+return (tt);
+}
+
